5d2701e66fbd3e2cf6c9cfc2345bbfaeff455804
Ushasri-Samala <ushasrisamala2803@gmail.com>
2023-10-11 07:45:40 +0000

    created simpleTodos

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,73 @@
+import './index.css'
+import {Component} from 'react'
+import TodoItem from '../TodoItem'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+class SimpleTodos extends Component {
+  state = {
+    todosList: initialTodosList,
+  }
+
+  onDeleteTodo = id => {
+    const {todosList} = this.state
+    const filteredTodos = todosList.filter(eachTodo => eachTodo.id !== id)
+    this.setState({todosList: filteredTodos})
+  }
+
+  render() {
+    const {todosList} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="todo-container">
+          <h1 className="header">Simple Todos</h1>
+          <ul className="todo-item-container">
+            {todosList.map(eachTodo => (
+              <TodoItem
+                todoItem={eachTodo}
+                key={eachTodo.id}
+                onDeleteTodo={this.onDeleteTodo}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
